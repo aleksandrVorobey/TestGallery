638307a8bdf824d59f943465cc665d6857971e0c
aleksandrVorobey <69941253+aleksandrVorobey@users.noreply.github.com>
2021-05-03 00:06:04 +0300

    addCollectionViewAndCell

---
+++ TestGallery/TestGallery/GalleryCells/ImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCell.swift
+//  TestGallery
+//
+//  Created by Александр Воробей on 02.05.2021.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TestGallery/TestGallery/GalleryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GalleryViewController.swift
+//  TestGallery
+//
+//  Created by Александр Воробей on 02.05.2021.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView.register(UINib(nibName: "ImageCell", bundle: nil), forCellWithReuseIdentifier: "ImageCell")
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+    }
+
+
+}
+
+extension GalleryViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
