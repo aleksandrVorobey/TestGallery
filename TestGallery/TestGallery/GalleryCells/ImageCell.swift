694ffb5b18195eb44b042b051887d9cff257071d
aleksandrVorobey <69941253+aleksandrVorobey@users.noreply.github.com>
2021-05-05 10:17:32 +0300

    add tappable links in viewCell

--- TestGallery/TestGallery/GalleryCells/ImageCell.swift
+++ TestGallery/TestGallery/GalleryCells/ImageCell.swift
@@ -11,9 +11,16 @@ class ImageCell: UICollectionViewCell {
     
     @IBOutlet weak var imageGallery: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var photoURL: UITextView!
+    @IBOutlet weak var nameURL: UITextView!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        photoURL.isEditable = false
+        nameURL.isEditable = false
+        photoURL.isHidden = true
+        nameURL.isHidden = true
     }
     
     override func prepareForReuse() {
@@ -22,12 +29,24 @@ class ImageCell: UICollectionViewCell {
         userNameLabel.text = nil
     }
     
+    
+    
     func configure(with urlString: String, userModel: GalleryModel) {
         DispatchQueue.global().async {
             guard let imageUrl = URL(string: urlString), let imageData = try? Data(contentsOf: imageUrl) else { return }
             DispatchQueue.main.async {
                 self.imageGallery.image = UIImage(data: imageData)
                 self.userNameLabel.text = userModel.userName
+                
+                let  stringPhotoURL = "PhotoURL"
+                let atributPhotoURL = NSMutableAttributedString(string: stringPhotoURL, attributes: [NSAttributedString.Key.link: URL(string: userModel.photoURL)!])
+                self.photoURL.attributedText = atributPhotoURL
+                self.photoURL.isHidden = false
+                
+                let stringNameURL = "NameURL"
+                let atributNameURL = NSMutableAttributedString(string: stringNameURL, attributes: [NSAttributedString.Key.link: URL(string: userModel.userURL)!])
+                self.nameURL.attributedText = atributNameURL
+                self.nameURL.isHidden = false
             }
         }
     }
